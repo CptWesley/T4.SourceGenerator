d2e32ce726a22fd759ea1429d6530e8cc62ef52d
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2024-09-03 20:13:06 +0200

    no message

---
+++ src/T4.SourceGenerator/DependencyResolver.cs
@@ -0,0 +1,52 @@
+﻿namespace T4.SourceGenerator;
+
+/// <summary>
+/// Provides a module initializer that configures the dependency resolver.
+/// </summary>
+internal static class DependencyResolver
+{
+    /// <summary>
+    /// Registers the dependency resolver.
+    /// </summary>
+    //[ModuleInitializer]
+    public static void RegisterDependencyResolver()
+    {
+        AppDomain.CurrentDomain.AssemblyResolve += (_, args) =>
+        {
+            return Load(args.Name);
+        };
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static Assembly? Load(string fullName)
+    {
+        var name = new AssemblyName(fullName);
+        var loadedAssembly = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.GetName().FullName == name.FullName);
+        if (loadedAssembly is { })
+        {
+            return loadedAssembly;
+        }
+
+        var currentAssembly = Assembly.GetExecutingAssembly();
+        var resourceName = $"T4.SourceGenerator.{name.Name}.dll";
+
+        using Stream resourceStream = currentAssembly.GetManifestResourceStream(resourceName);
+
+        var success = resourceStream is { };
+#pragma warning disable
+        File.WriteAllText(@$"D:\Git\T4.SourceGenerator\src\T4.SourceGenerator\bin\Release\foo-{Guid.NewGuid().ToString("N")}.txt", $"Looking for (Success={success}): '{resourceName}'. Found: [\n{string.Join("\n", currentAssembly.GetManifestResourceNames())}\n]");
+
+        if (resourceStream is null)
+        {
+            return null;
+        }
+
+        using MemoryStream memoryStream = new MemoryStream();
+        resourceStream.CopyTo(memoryStream);
+        var bytes = memoryStream.ToArray();
+#pragma warning disable RS1035
+        var loaded = Assembly.Load(bytes);
+#pragma warning restore RS1035
+        return loaded;
+    }
+}
--- src/T4.SourceGenerator/T4Generator.cs
+++ src/T4.SourceGenerator/T4Generator.cs
@@ -33,9 +33,32 @@ public sealed class T4Generator : IIncrementalGenerator
         }
         catch (Exception e)
         {
-            ctx.ReportDiagnostic(Diagnostics.ExceptionError, file.GetLocation(0, 0), file.Path, e.ToString());
+            var escaped = e.ToString().Replace("\n", "\\n").Replace("\r", "\\r").Replace("\t", "\\t");
+            ctx.ReportDiagnostic(Diagnostics.ExceptionError, file.GetLocation(0, 0), file.Path, escaped);
         }
     }
+    /*
+    private static void Unpack(StringBuilder sb, Exception e, int depth = 0)
+    {
+        if (e is AggregateException ae)
+        {
+            sb.AppendLine(ae.inn);
+        }
+    }
+
+    private static void Print(StringBuilder sb, Exception e, int depth)
+    {
+
+    }
+
+    private static string Pad(string text, int depth)
+    {
+        var padding = new string(' ', depth * 2);
+        var lines = Regex.Split(text, @"\r\n|\r|\n");
+        var modified = lines.Select(line => padding + line);
+        var joined = string.Join(Environment.NewLine, )
+    }
+    */
 
     private static bool Generate(ref SourceProductionContext ctx, AdditionalText file)
     {
@@ -59,6 +82,25 @@ public sealed class T4Generator : IIncrementalGenerator
         var settings = TemplatingEngine.GetSettings(generator, parsed);
         settings.CompilerOptions = "-nullable:enable";
 
+#pragma warning disable
+        var refs = generator.Refs.Select(x =>
+        {
+            if (x is null)
+            {
+                return "null";
+            }
+            else
+            {
+                return $"\"{x}\"";
+            }
+        });
+
+        generator.Refs.Add(GetAssemblyName<Mono.TextTemplating.CompiledTemplate>());
+        generator.Refs.Add(GetAssemblyName(typeof(Mono.TextTemplating.RoslynTemplatingEngineExtensions)));
+        generator.Refs.Add(GetAssemblyName<System.CodeDom.CodeBinaryOperatorExpression>());
+        generator.Refs.RemoveAll(x => string.IsNullOrWhiteSpace(x));
+
+        File.WriteAllText($@"D:\Git\T4.SourceGenerator\src\T4.SourceGenerator\bin\Release\refs-{Guid.NewGuid().ToString("N")}.txt", string.Join(Environment.NewLine, refs));
         (var outputName, var content) = generator.ProcessTemplateAsync(parsed, templateName, templateContent, templateName, settings).Result;
 
         messages = GetMessages(generator.Errors);
@@ -72,6 +114,19 @@ public sealed class T4Generator : IIncrementalGenerator
         ctx.AddSource(Path.GetFileName(outputName), content);
         return true;
     }
+
+    private static string GetAssemblyName(Type type)
+    {
+        var path = type.Assembly.Location?.Trim();
+        if (string.IsNullOrWhiteSpace(path))
+        {
+            path = type.Assembly.FullName;
+        }
+        return path;
+    }
+
+    private static string GetAssemblyName<T>()
+        => GetAssemblyName(typeof(T));
 
     private static IReadOnlyList<CompilerError> GetMessages(CompilerErrorCollection errors)
     {
